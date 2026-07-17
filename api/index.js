74807f634b53e8db875fce9b9d24d77e2e00cd45
steamonground-beep <steamonground-beep@users.noreply.github.com>
2026-07-17 12:35:32 -0500

    Fix Vercel deployment: add package.json, use CommonJS export

--- api/index.js
+++ api/index.js
@@ -478,7 +478,7 @@ function logActivity(data, action, detail) {
 }
 
 // â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
